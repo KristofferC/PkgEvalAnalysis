da6fc90279ec047427d01e39f1f2a005e85cec66
Kristoffer <kcarlsson89@gmail.com>
2023-02-03 07:33:40 +0100

    update

--- add_back_logs.jl
+++ add_back_logs.jl
@@ -5,7 +5,7 @@
 
     short_sha(str) = str[1:7]
 
-    function add_back_logs(path)
+    function add_back_logs(path; date=nothing)
         # Primary
         json_primary = JSON.parsefile(joinpath(path, "primary.json"))
         sha_primary = short_sha(json_primary["build"]["sha"])
@@ -13,7 +13,12 @@
         json_against = JSON.parsefile(joinpath(path, "against.json"))
         sha_against = short_sha(json_against["build"]["sha"])
 
-        base_url = "https://s3.amazonaws.com/julialang-reports/nanosoldier/pkgeval/by_hash/$(sha_primary)_vs_$(sha_against)/"
+        if date == nothing
+            postfix = "by_hash/$(sha_primary)_vs_$(sha_against)"
+        else
+            postfix = "by_date/$date"
+        end
+        base_url = "https://s3.amazonaws.com/julialang-reports/nanosoldier/pkgeval/$postfix/"
 
         add_back_log(path, base_url, :primary)
         add_back_log(path, base_url, :against)
@@ -36,9 +41,10 @@
             @async while !isempty(queue)            
                 package = popfirst!(queue)
                 output = IOBuffer()
-                r = Downloads.request(string(base_url, string(package, ".", type, ".log")) ;output)
+                url = string(base_url, string(package, ".", type, ".log"))
+                r = Downloads.request(url ;output)
                 if r.status !== 200
-                    @warn "Failed to get log for package $(repr(package)) from url $(repr(base_url))"
+                    # @warn "Failed to get log for package $(repr(package)) from url $(repr(url))"
                     d[package] = ""
                 else
                     d[package] = String(take!(output))
